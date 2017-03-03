4dc1a0a84b51bdcecfb860889c82b726bdb56694
Trevor Hamilton <tjh1003@sru.edu>
2017-03-02 20:46:55 -0500

    Functional dynamic mouse coloring
    Exposure of most of the functionallity of the mouse lighting types.
    Going to work on the other device types next.
    
    (I had intended to have a commit before this one but I never hit enter I
    guess :O )

--- RGBControllerPlugin.cs
+++ RGBControllerPlugin.cs
@@ -12,24 +12,125 @@ namespace PluginRGBController
 {
     internal class Measure
     {
+        enum effectTypes
+        {
+            STATIC,
+            BREATHING,
+            //BLINKING,
+            WAVE,
+            SPECTRUM,
+            REACTIVE
+        }
+        enum deviceTypes
+        {
+            MOUSE
+        }
+
         internal Measure()
         {
-            Corale.Colore.Core.Mouse.Instance.SetStatic(new Static(Led.All, Corale.Colore.Core.Color.Red));
         }
 
         internal void Reload(Rainmeter.API api, ref double maxValue)
         {
-            Corale.Colore.Core.Mouse.Instance.SetStatic(new Static(Led.All, Corale.Colore.Core.Color.Blue));
+            String RGB = api.ReadString("Color", null);
+            String RGB2 = api.ReadString("Color2", null);
+
+            if (RGB != null)
+            {
+                Color RGBColor, RGBColor2 = new Color();
+
+                try
+                {
+                    {
+                        String[] RGBarr = RGB.Split(',');
+                        byte R = Convert.ToByte(RGBarr[0]);
+                        byte G = Convert.ToByte(RGBarr[1]);
+                        byte B = Convert.ToByte(RGBarr[2]);
+                        RGBColor = new Color(R, G, B);
+                    }
+
+                    if (RGB2 != null)
+                    {
+                        String[] RGBarr = RGB2.Split(',');
+                        byte R = Convert.ToByte(RGBarr[0]);
+                        byte G = Convert.ToByte(RGBarr[1]);
+                        byte B = Convert.ToByte(RGBarr[2]);
+                        RGBColor2 = new Color(R, G, B);
+                    }
+                }
+                catch
+                {
+                    API.Log(API.LogType.Error, "RGB Value(s) are malformed, correct form is R,G,B");
+                    return;
+                }
+                String effect = api.ReadString("Effect", "static").ToUpper();
+                String device = api.ReadString("Device", "all").ToUpper();
+
+                if (effect.CompareTo(effectTypes.STATIC.ToString()) == 0)
+                {
+                    if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                    {
+                        Mouse.Instance.SetStatic(new Static(Led.All, RGBColor));
+                    }
+                }
+                else if (effect.CompareTo(effectTypes.BREATHING.ToString()) == 0)
+                {
+                    if (RGB2 == null)
+                    {
+                        if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                        {
+                            Mouse.Instance.SetBreathing(new Breathing(Led.All, RGBColor));
+                        }
+                    }
+                    else
+                    {
+                        if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                        {
+                            Mouse.Instance.SetBreathing(new Breathing(Led.All, RGBColor, RGBColor2));
+                        }
+                    }
+                }
+                //else if (effect.CompareTo(effectTypes.BLINKING.ToString()) == 0)
+                //{
+                //    if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                //    {
+                //        Mouse.Instance.SetBlinking(new Blinking(Led.All, RGBColor));
+                //    }
+                //}
+                else if (effect.CompareTo(effectTypes.SPECTRUM.ToString()) == 0)
+                {
+                    if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                    {
+                        Mouse.Instance.SetSpectrumCycling(new SpectrumCycling(Led.All));
+                    }
+                }
+                else if (effect.CompareTo(effectTypes.REACTIVE.ToString()) == 0)
+                {
+                    if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                    {
+                        //TODO Add user defined duration
+                        Mouse.Instance.SetReactive(new Reactive(Led.All, Duration.Long, RGBColor));
+                    }
+                }
+                else if (effect.CompareTo(effectTypes.WAVE.ToString()) == 0)
+                {
+                    if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                    {
+                        //TODO Add user defined direction
+                        Mouse.Instance.SetWave(new Wave(Direction.FrontToBack));
+                    }
+                }
+            }
         }
 
         internal double Update()
         {
-            return 0.0;
+            return Color.Blue;
         }
 
         internal string GetString()
         {
-            return "";
+            return Color.Blue.ToString();
         }
 
         internal void ExecuteBang(string args)
