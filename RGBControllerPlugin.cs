b766926240b3fa937ca9a01ac029358b58053eec
Trevor Hamilton <tjh1003@sru.edu>
2017-03-02 23:12:42 -0500

    Addedkeyboard support
    Also moved the color stuff to its own function to make adding bang
    support easier.

--- RGBControllerPlugin.cs
+++ RGBControllerPlugin.cs
@@ -11,6 +11,9 @@ using Corale.Colore.Razer.Mouse.Effects;
 using Corale.Colore.Razer.Headset;
 using Corale.Colore.Razer.Headset.Effects;
 
+using Corale.Colore.Razer.Keyboard;
+using Corale.Colore.Razer.Keyboard.Effects;
+
 namespace PluginRGBController
 {
     internal class Measure
@@ -27,26 +30,56 @@ namespace PluginRGBController
         enum deviceTypes
         {
             MOUSE,
-            HEADSET
+            HEADSET,
+            KEYBOARD
+        }
+        enum keyboardGroups
+        {
+            MAIN, //The normal keys
+            MAINNOWASD, //Normal keys not including WASD
+            WASD, //W A S and D keys
+            NUMPAD, //Numpad
+            ARROWS, //Arrow keys
+            FUNCTION, //The 12 function keys and the escape button
+            SYSTEM, //The 9 system keys (insert, home, pg up, delete, end, pg down, print screen, scroll lock and break) 
+            EXTRAS, //Any extra keys your keyboard has, if applicable
+            CUSTOM //A set of custom keys to change defined in the rainmeter measure using the option keylist (comma or space seperated)
         }
 
         String currentColor = "";
 
-        internal Measure()
+        void UpdateColor(String RGB, String RGB2, String effect, String device)
         {
-        }
 
-        internal void Reload(Rainmeter.API api, ref double maxValue)
-        {
-            String RGB = api.ReadString("Color", null);
-            String RGB2 = api.ReadString("Color2", null);
-
-            String effect = api.ReadString("Effect", "static").ToUpper();
-            String device = api.ReadString("Device", "all").ToUpper();
-
-            currentColor = RGB;
-
-            if (RGB != null && RGB != "")
+            if (effect.CompareTo(effectTypes.SPECTRUM.ToString()) == 0)
+            {
+                if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                {
+                    Mouse.Instance.SetSpectrumCycling(new SpectrumCycling(Led.All));
+                }
+                else if (device.CompareTo(deviceTypes.HEADSET.ToString()) == 0)
+                {
+                    Headset.Instance.SetEffect(Corale.Colore.Razer.Headset.Effects.Effect.SpectrumCycling);
+                }
+                else if (device.CompareTo(deviceTypes.KEYBOARD.ToString()) == 0)
+                {
+                    Keyboard.Instance.SetEffect(Corale.Colore.Razer.Keyboard.Effects.Effect.SpectrumCycling);
+                }
+            }
+            else if (effect.CompareTo(effectTypes.WAVE.ToString()) == 0)
+            {
+                if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                {
+                    //TODO Add user defined direction
+                    Mouse.Instance.SetWave(new Corale.Colore.Razer.Mouse.Effects.Wave(Corale.Colore.Razer.Mouse.Effects.Direction.FrontToBack));
+                }
+                else if (device.CompareTo(deviceTypes.KEYBOARD.ToString()) == 0)
+                {
+                    //TODO Add user defined direction
+                    Keyboard.Instance.SetWave(new Corale.Colore.Razer.Keyboard.Effects.Wave(Corale.Colore.Razer.Keyboard.Effects.Direction.LeftToRight));
+                }
+            }
+            else if (RGB != null && RGB != "")
             {
                 Color RGBColor, RGBColor2 = new Color();
 
@@ -81,9 +114,13 @@ namespace PluginRGBController
                     {
                         Mouse.Instance.SetStatic(new Corale.Colore.Razer.Mouse.Effects.Static(Led.All, RGBColor));
                     }
-                    else if(device.CompareTo(deviceTypes.HEADSET.ToString()) == 0)
+                    else if (device.CompareTo(deviceTypes.HEADSET.ToString()) == 0)
                     {
                         Headset.Instance.SetStatic(new Corale.Colore.Razer.Headset.Effects.Static(RGBColor));
+                    }
+                    else if (device.CompareTo(deviceTypes.KEYBOARD.ToString()) == 0)
+                    {
+                        Keyboard.Instance.SetStatic(new Corale.Colore.Razer.Keyboard.Effects.Static(RGBColor));
                     }
                 }
                 else if (effect.CompareTo(effectTypes.BREATHING.ToString()) == 0)
@@ -107,6 +144,22 @@ namespace PluginRGBController
                     {
                         Headset.Instance.SetBreathing(new Corale.Colore.Razer.Headset.Effects.Breathing(RGBColor));
                     }
+                    else if (device.CompareTo(deviceTypes.KEYBOARD.ToString()) == 0)
+                    {
+                        if (RGB2 == null || RGB2 == "")
+                        {
+                            //For some odd reason keyboard breathing effect only takes two colors
+                            Keyboard.Instance.SetBreathing(new Corale.Colore.Razer.Keyboard.Effects.Breathing(RGBColor, RGBColor));
+                        }
+                        else
+                        {
+                            currentColor += ":" + RGB2;
+                            if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                            {
+                                Keyboard.Instance.SetBreathing(new Corale.Colore.Razer.Keyboard.Effects.Breathing(RGBColor, RGBColor2));
+                            }
+                        }
+                    }
                 }
                 //else if (effect.CompareTo(effectTypes.BLINKING.ToString()) == 0)
                 //{
@@ -120,29 +173,32 @@ namespace PluginRGBController
                     if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
                     {
                         //TODO Add user defined duration
-                        Mouse.Instance.SetReactive(new Reactive(Led.All, Duration.Long, RGBColor));
+                        Mouse.Instance.SetReactive(new Corale.Colore.Razer.Mouse.Effects.Reactive(Led.All, Corale.Colore.Razer.Mouse.Effects.Duration.Long, RGBColor));
                     }
-                }
-                else if (effect.CompareTo(effectTypes.WAVE.ToString()) == 0)
-                {
-                    if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
+                    else if (device.CompareTo(deviceTypes.KEYBOARD.ToString()) == 0)
                     {
-                        //TODO Add user defined direction
-                        Mouse.Instance.SetWave(new Wave(Direction.FrontToBack));
+                        //TODO Add user defined duration
+                        Keyboard.Instance.SetReactive(new Corale.Colore.Razer.Keyboard.Effects.Reactive(RGBColor, Corale.Colore.Razer.Keyboard.Effects.Duration.Long));
                     }
                 }
             }
-            else if (effect.CompareTo(effectTypes.SPECTRUM.ToString()) == 0)
-            {
-                if (device.CompareTo(deviceTypes.MOUSE.ToString()) == 0)
-                {
-                    Mouse.Instance.SetSpectrumCycling(new SpectrumCycling(Led.All));
-                }
-                else if (device.CompareTo(deviceTypes.HEADSET.ToString()) == 0)
-                {
-                    Headset.Instance.SetEffect(Corale.Colore.Razer.Headset.Effects.Effect.SpectrumCycling);
-                }
-            }
+        }
+
+        internal Measure()
+        {
+        }
+
+        internal void Reload(Rainmeter.API api, ref double maxValue)
+        {
+            String RGB = api.ReadString("Color", null);
+            String RGB2 = api.ReadString("Color2", null);
+
+            String effect = api.ReadString("Effect", "static").ToUpper();
+            String device = api.ReadString("Device", "all").ToUpper();
+
+            currentColor = RGB;
+
+            UpdateColor(RGB, RGB2, effect, device);
         }
 
         internal double Update()
